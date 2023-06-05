1c9e61c7855dfa5cd7aeb708d877fae1ba729475
mcamaya <camiamayar@gmail.com>
2023-06-05 13:23:00 -0500

    feat: :sparkles: Validando datos -> filter_var

---
+++ validar-datos/server.php
@@ -0,0 +1,30 @@
+<?php
+
+$is_float = filter_var("esto no es un float", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_THOUSAND);
+$is_int = filter_var("asdfghjkl", FILTER_VALIDATE_INT);
+$is_ip = filter_var("0.0.0", FILTER_VALIDATE_IP);
+$is_url = filter_var("platzi.com", FILTER_VALIDATE_URL);
+$is_email = filter_var("no es un email", FILTER_VALIDATE_EMAIL);
+
+echo "<h2>Valores no válidos </h2><pre>";
+var_dump($is_float);
+var_dump($is_int);
+var_dump($is_ip);
+var_dump($is_url);
+var_dump($is_email);
+echo "</pre>";
+
+
+$is_float = filter_var("0.9", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_THOUSAND);
+$is_int = filter_var("80", FILTER_VALIDATE_INT);
+$is_ip = filter_var("127.0.0.1", FILTER_VALIDATE_IP);
+$is_url = filter_var("https://platzi.com", FILTER_VALIDATE_URL);
+$is_email = filter_var("mrmichi@michisancio.com", FILTER_VALIDATE_EMAIL);
+
+echo "<h2>Valores válidos </h2><pre>";
+var_dump($is_float);
+var_dump($is_int);
+var_dump($is_ip);
+var_dump($is_url);
+var_dump($is_email);
+echo "</pre>";
