3d406dd80225bf94c52b3c9d99cbdfe7b2fc4adc
mcamaya <camiamayar@gmail.com>
2023-06-05 19:43:48 -0500

    test: :tada: Validando emails - Instalando dependencias con Composer y PHPUnit
    Hace falta repasar conocimientos

---
+++ proyecto/tests/ValidateTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use App\Validate;
+
+class ValidateTest extends TestCase {
+    public function test_email(){
+        $email = Validate::email('i@imorsoft.com');
+        $this->assertTrue($email);
+
+        $email = Validate::email('i@@imorsoft.com');
+        $this->assertFalse($email);
+    }
+}
