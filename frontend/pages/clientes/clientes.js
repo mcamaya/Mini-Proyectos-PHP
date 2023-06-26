0328885e44a171757770e69a797d48491d88bb35
mcamaya <camiamayar@gmail.com>
2023-06-26 12:46:55 -0500

    feat: :sparkles: Mostramos los datos en pantalla

--- frontend/pages/clientes/clientes.js
+++ frontend/pages/clientes/clientes.js
@@ -7,4 +7,16 @@ async function cargaClientes() {
     const clientes = await getClientes();
     console.log(clientes);
     const tablaClientes = document.querySelector('#datosClientes');
+    clientes.forEach(cliente => {
+        tablaClientes.innerHTML += `
+        <tr>
+            <td>${cliente.id_constructora}</td>
+            <td>${cliente.nombre_constructora}</td>
+            <td>${cliente.nit_constructora}</td>
+            <td>${cliente.nombre_representante}</td>
+            <td>${cliente.email_contacto}</td>
+            <td>${cliente.telefono_contacto}</td>
+        </tr>
+        `;
+    });
 }
