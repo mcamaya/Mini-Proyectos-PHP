9a3c85d52cc53443feb4b4c5ba17b782351832ab
mcamaya <camiamayar@gmail.com>
2023-06-27 22:26:38 -0500

    Mostramos datos en pantalla

--- frontend/pages/clientes/clientes.js
+++ frontend/pages/clientes/clientes.js
@@ -1,8 +1,24 @@
 import { getClientes } from "./API.JS";
 
 addEventListener("DOMContentLoaded", cargarClientes);
+const tabla = document.querySelector('#datosClientes');
 
 async function cargarClientes(){
-    const result = await getClientes();
-    console.log(result);
+    const clientes = await getClientes();
+    console.log(clientes);
+
+    clientes.forEach(clt => {
+        tabla.innerHTML += `
+            <tr>
+                <td>${clt.id_constructora}</td>
+                <td>${clt.nombre_constructora}</td>
+                <td>${clt.nit_constructora}</td>
+                <td>${clt.nombre_representante}</td>
+                <td>${clt.email_contacto}</td>
+                <td>${clt.telefono_contacto}</td>
+            </tr>
+        `        
+    });
 }
+
+
